7511795d07c17b05cef412cbfd622874e94ada5e
Tayler <emontesinos812@gmail.com>
2026-08-17 08:55:24 -0500

    update text notify

--- composeApp/src/androidMain/kotlin/com/tayler/pizzzaapp/service/MyFirebaseMessagingService.kt
+++ composeApp/src/androidMain/kotlin/com/tayler/pizzzaapp/service/MyFirebaseMessagingService.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.speech.tts.TextToSpeech
-import android.speech.tts.Voice
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
