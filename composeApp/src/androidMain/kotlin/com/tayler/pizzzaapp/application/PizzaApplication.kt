bce5d98c8748fc09c422b308c071433cafe6b991
Tayler <emontesinos812@gmail.com>
2026-08-25 10:31:37 -0500

    optimize code

--- composeApp/src/androidMain/kotlin/com/tayler/pizzzaapp/application/PizzaApplication.kt
+++ composeApp/src/androidMain/kotlin/com/tayler/pizzzaapp/application/PizzaApplication.kt
@@ -1,27 +1,21 @@
 package com.tayler.pizzzaapp.application
 
 import android.app.Application
-import com.tayler.pizzzaapp.di.dispatcherModule
-import com.tayler.pizzzaapp.repository.di.networkModule
-import com.tayler.pizzzaapp.repository.di.dbModule
+import com.tayler.pizzzaapp.di.initKoin
 import com.tayler.pizzzaapp.di.viewModelModule
-import com.tayler.pizzzaapp.repository.di.repositoryModule
-import com.tayler.pizzzaapp.usecases.di.useCasesModule
 import com.tayler.pizzzaapp.utils.NotificationHelper
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
 
 class PizzaApplication: Application()  {
 
     override fun onCreate() {
         super.onCreate()
         NotificationHelper.createNotificationChannel(this)
-        startKoin {
+        initKoin {
             androidContext(this@PizzaApplication)
             androidLogger()
-            modules( dispatcherModule,viewModelModule,
-                repositoryModule, useCasesModule, networkModule, dbModule)
+            modules(viewModelModule)
         }
     }
 }
